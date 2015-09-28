1caa85abcab810f78d7a03510e7da8457070668b
Damien Bachet <dbachet@gmail.com>
2015-09-29 00:50:27 +0200

    Add ability to logout

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -5,6 +5,10 @@ export default Ember.Controller.extend({
     authenticate: function() {
       var credentials = this.getProperties('identification', 'password');
       this.get('session').authenticate('simple-auth-authenticator:devise', credentials);
+    },
+
+    invalidateSession: function() {
+      this.get('session').invalidate();
     }
   }
 });
