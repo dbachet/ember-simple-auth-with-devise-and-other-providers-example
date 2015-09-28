06829f540b60f37e41c15e288b327ecef0c2e2d7
Damien Bachet <dbachet@gmail.com>
2015-09-29 01:33:48 +0200

    Add a protected route

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -5,10 +5,6 @@ export default Ember.Controller.extend({
     authenticate: function() {
       var credentials = this.getProperties('identification', 'password');
       this.get('session').authenticate('simple-auth-authenticator:devise', credentials);
-    },
-
-    invalidateSession: function() {
-      this.get('session').invalidate();
     }
   }
 });
