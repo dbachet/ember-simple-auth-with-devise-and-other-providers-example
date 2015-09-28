a92abb4512decd246bb52b177a99aa1f705b7876
Damien Bachet <dbachet@gmail.com>
2015-09-29 01:44:45 +0200

    Display error message on authentication failure

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,8 +3,12 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     authenticate: function() {
+      var _this = this;
       var credentials = this.getProperties('identification', 'password');
-      this.get('session').authenticate('simple-auth-authenticator:devise', credentials);
+      this.get('session').authenticate('simple-auth-authenticator:devise', credentials).then(null, function(error) {
+        var message = error.error;
+        _this.set('errorMessage', message);
+      });;
     }
   }
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(UnauthenticatedRouteMixin);
+// The login route only accessible when the session is not authenticated
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
+  // Clear a potentially stale error message from previous login attempts
+  setupController: function(controller, model) {
+    controller.set('errorMessage', null);
+  }
+});
